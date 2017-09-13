cab0e8dacd88f94766f40e41bc86cedc382eef10
Victor Aurélio <ovictoraurelio@gmail.com>
2017-09-13 20:41:48 -0300

    added nodeJS

--- env.sh
+++ env.sh
@@ -28,21 +28,22 @@ function error {
     printf "\n\n\t${red}$1$2$3${wht}"
 }
 function settings {	
-	wht=$(tput sgr0);red=$(tput setaf 1);gre=$(tput setaf 2);yel=$(tput setaf 3);blu=$(tput setaf 4);
-	if [ ! -d $HOME/apps ]; then 
-		doing "Creating apps folder"
-		mkdir $HOME/apps
-	fi
-	DIR=$HOME/apps
+    chmod 700 $HOME    
+    gsettings reset com.canonical.Unity.Launcher favorites
+    gsettings set com.canonical.Unity.Launcher favorites "$(
+python << EOF
+array = eval("$(gsettings get com.canonical.Unity.Launcher favorites)")
+print(str(array[:3] + array[8:]))
+EOF
+)"	
 }
 function backgroundImage {
     wget -O $DIR/background.jpg http://cin.ufpe.br/~vags/tardis_wallpaper.jpg
     gsettings set org.gnome.desktop.background picture-uri file://$DIR/background.jpg
 }
 function addShortcut {
-    doing "installing telegram"
-    echo "$HOME/.local/share/applications/$1"
-
+    doing "adding shortcut for $1"
+    
     gtk-update-icon-cache -f $HOME/.local/share/icons/hicolor >/dev/null 2>&1
 	desktop-file-install --dir=$HOME/.local/share/applications "$HOME/.local/share/applications/$1"
     
@@ -52,11 +53,10 @@ from collections import OrderedDict
 array = eval("$(gsettings get com.canonical.Unity.Launcher favorites)")
 print(str(list(OrderedDict.fromkeys(array[:-3] + ["$1"] + array[-3:]))))
 EOF
-)    
-	gsettings set com.canonical.Unity.Launcher favorites "$favorites"
+)
+    gsettings set com.canonical.Unity.Launcher favorites "$favorites"
     
-    finished "wow"
-    echo "$favorites"
+    finished "$1 added to sidebar"    
 }
 
 function telegram {
@@ -83,13 +83,28 @@ function code {
 	$DIR/code/code &
 	finished "VS Code successfully installed\n"
 }
+function copy {
+    ( cp -fRv ${@:2} | pv -elnps $(find ${@:2:(( $# - 2 ))} | wc -l) ) 2>&1 | whiptail --title "Extracting" --gauge "\nStatus complete$1..." 0 0 0
+}
+
+function nodeJS {    
+    doing "installing Node.js...\n"
+    wget -O $DIR/node.txz https://nodejs.org/dist/v6.11.3/node-v6.11.3-linux-x64.tar.xz     
+    pv -n $DIR/node.txz  | tar -xJf - -C $DIR/apps/node/ 
+    copy "Node.js (part 1/4)" $DIR/apps/node/*/bin $HOME/teste
+    copy "Node.js (part 2/4)" $DIR/apps/node/*/include $HOME/teste
+    copy "Node.js (part 3/4)" /tmp/node*/lib $HOME/.local
+    copy "Node.js (part 4/4)" /tmp/node*/share $HOME/.local
+
+    finished "Node.js installed successfully!"
+}
 
 function configTelegram {
     cat << EOF > $HOME/.local/share/applications/telegram.desktop
 [Desktop Entry]
 Version=1.0
-Name=Telegram Desktop
-Comment=Official desktop version of Telegram messaging app
+Name=Telegram D
+Comment= desktop version of Telegram app
 TryExec=$DIR/telegram/Telegram
 Exec=$DIR/telegram/Telegram -- %u
 Icon=telegram
@@ -99,49 +114,54 @@ Type=Application
 Categories=Network;InstantMessaging;Qt;
 MimeType=x-scheme-handler/tg;
 X-Desktop-File-Install-Version=0.22
-EOF    
+EOF 
     addShortcut telegram.desktop
 }
 
 
 function configCode {
 	cat << EOF > $HOME/.config/Code/User/settings.json	
+// Place your settings in this file to overwrite the default settings
 {
     "files.autoSave": "off",
     "workbench.colorTheme": "Northem Dark",
     "workbench.iconTheme": "material-icon-theme",
     "sync.gist": "47639fa50f09b6dc7d21d33dc7922860",
-    "sync.lastUpload": "2017-01-09T00:50:26.952Z",
+    "sync.lastUpload": "2017-01-13T11:38:00.848Z",
     "sync.autoDownload": true,
     "sync.autoUpload": false,
-    "sync.lastDownload": "2017-01-09T00:40:19.423Z",
+    "sync.lastDownload": "2017-01-13T11:38:00.848Z",
     "sync.forceDownload": false,
     "sync.anonymousGist": false,
     "sync.host": "",
     "sync.pathPrefix": "",
     "sync.quietSync": false,
-    "sync.askGistName": false,                      
+    "sync.askGistName": false,
+    "explorer.autoReveal": false,
     "workbench.activityBar.visible": true,
     "files.associations": {
         "*.twig": "html",
-        "*.html": "twig"
+        "*.html": "html"
     },
     "extensions.ignoreRecommendations": false,
-    "window.zoomLevel": 0
+    "window.zoomLevel": 0,
+    "workbench.quickOpen.closeOnFocusLost": false
 }
 EOF
+    addShortcut code.desktop
 	cat << EOF > $HOME/.config/Code/User/keybindings.json	
+// Empty
 [
-    { "key": "ctrl+\\\",              "command": "workbench.action.toggleSidebarVisibility" },
-    { "key": "ctrl+k ctrl+up",        "command": "workbench.action.splitEditor" },
-    { "key": "ctrl+t",                "command": "workbench.action.quickOpen" },
-    { "key": "ctrl+e",                "command": "workbench.action.showAllSymbols" },
-    { "key": "ctrl+j", 				  "command": "workbench.action.terminal.toggleTerminal" }
+    { "key": "ctrl+\\\\",                 "command": "workbench.action.toggleSidebarVisibility" },
+    { "key": "ctrl+k ctrl+up",          "command": "workbench.action.splitEditor" },
+    { "key": "ctrl+t",                  "command": "workbench.action.quickOpen" },
+    { "key": "ctrl+e",                  "command": "workbench.action.showAllSymbols" },
+    { "key": "ctrl+j",                  "command": "workbench.action.terminal.toggleTerminal" },
+    { "key": "ctrl+shift+i",            "command": "HookyQR.beautifyFile"},
+    { "key": "ctrl+shift+c",          "command": "editor.action.blockComment"}
 ]
 EOF
-
     cat << EOF > $HOME/.local/share/applications/code.desktop
-
 [Desktop Entry]
 Version=1.0
 Name=code
@@ -156,14 +176,23 @@ Categories=Development;IDE;
 MimeType=x-scheme-handler/tg;
 X-Desktop-File-Install-Version=0.22
 EOF
-    
-    addShortcut code.desktop
+}
+
+function essentials {
+    wht=$(tput sgr0);red=$(tput setaf 1);gre=$(tput setaf 2);yel=$(tput setaf 3);blu=$(tput setaf 4);
+    if [ ! -d $HOME/apps ]; then 
+        doing "Creating apps folder"
+        mkdir $HOME/apps
+    fi
+    DIR=$HOME/apps
 }
 
 function MAIN {	
+    essentials
 	settings
     backgroundImage
 	telegram
     code
+    nodeJS
 }
 MAIN
